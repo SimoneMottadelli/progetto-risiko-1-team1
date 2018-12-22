ce1bf23b152a351625b7f80d98411eea53875c1e
claudio <c.rota30@campus.unimib.it>
2018-12-22 23:55:44 +0100

    test git - created some methods in Map class

--- src/main/java/com/drisk/domain/Map.java
+++ src/main/java/com/drisk/domain/Map.java
@@ -2,6 +2,22 @@ package com.drisk.domain;
 
 public class Map {
 
+	private Continent[] continents;
+
+	public Map() {
+		continents = new Continent[10];
+	}
 	
+	public Map(Continent[] continents) {
+		this.continents = continents;
+	}
+
+	public Continent[] getContinents() {
+		return continents;
+	}
+
+	public void setContinents(Continent[] continents) {
+		this.continents = continents;
+	}
 	
 }
