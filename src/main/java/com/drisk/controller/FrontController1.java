d1c77d9ac78e638602d0b6ef06bbe038c04d6f76
claudio <c.rota30@campus.unimib.it>
2018-12-22 22:55:54 +0100

    init commit

---
+++ src/main/java/com/drisk/controller/FrontController1.java
@@ -0,0 +1,16 @@
+package com.drisk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FrontController1 {
+
+	@RequestMapping(value="/prova1")
+	@ResponseBody
+	public String Prova() {
+		return "ciao1";
+	}
+	
+}
