af38cd82519e0a979baa611db9222047abe703b0
SimoneMottadelli <s.mottadelli2@campus.unimib.it>
2018-12-29 01:18:41 +0100

    trying request-body handling in MatchController

--- src/main/java/com/drisk/controller/MatchController.java
+++ src/main/java/com/drisk/controller/MatchController.java
@@ -1,27 +1,70 @@
 package com.drisk.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.drisk.domain.MatchManager;
+import com.drisk.domain.Turn;
 
 @Controller
 public class MatchController {
 	
 	private int x = 0;
 	
-	@GetMapping(value="/join")
+	@PostMapping(value="/join")
 	@ResponseBody
-	public String join() {
+	public String join(HttpServletRequest request, HttpServletResponse response) {
 		MatchManager mm = MatchManager.getInstance();
 		if(mm.isMatchStarted())
 			return "match started";
 		else if(mm.isMatchFull())
 			return "match full";
 		else {
-			mm.joinGame("Player" + x++);
-			return "ok";
+			String playerName = request.getParameter("name");
+			mm.joinGame(playerName);
+			x++;
+			return  playerName + " has joined the game! Number of players: " + x;
 		}
 	}
+	
+	
+	@GetMapping(value="/startGame")
+	@ResponseBody
+	public String startGame() {
+		MatchManager mm = MatchManager.getInstance();
+		if (!mm.isMatchStarted()) {
+			mm.startGame();
+			return "Game's just started!";
+		}
+		return "The game has already begun!";
+	}
+	
+	
+	@GetMapping(value="/nextPhase")
+	@ResponseBody
+	public String nextPhase() {
+		MatchManager mm = MatchManager.getInstance();
+		if (mm.isMatchStarted()) {
+			Turn.getInstance().getCurrentPhase().nextPhase();
+			return "Going to the next phase!";
+		}
+		return "Couldn't go to the next phase!";
+	}
+	
+	
+	@GetMapping(value="/playPhase")
+	@ResponseBody
+	public String playPhase() {
+		MatchManager mm = MatchManager.getInstance();
+		if (mm.isMatchStarted()) {
+			Turn.getInstance().getCurrentPhase().startPhase();
+			return "playing the phase...";
+		}
+		return "Couldn't play the phase!";
+	}
+	
 }
