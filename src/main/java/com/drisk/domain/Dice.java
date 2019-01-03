cc70f189f77cc1920fbde4e9485f1fe10ad7f8f6
Stefano Zuccarella <s.zuccarella1@campus.unimib.it>
2019-01-03 20:02:44 +0100

    Updating AttackPhase and Dice classes. Implement findPlayer method in Territory class

--- src/main/java/com/drisk/domain/Dice.java
+++ src/main/java/com/drisk/domain/Dice.java
@@ -3,6 +3,9 @@ package com.drisk.domain;
 import java.security.SecureRandom;
 import java.util.Arrays; 
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.lang.Math;
 
 public class Dice {
 	
@@ -10,31 +13,39 @@ public class Dice {
 		return new SecureRandom().nextInt(6) + 1;
 	}
 	
-	public int rollDices(int attackerTanks, int defenderTanks) {
+	public int[] rollDices(int attackerTanks, int defenderTanks) {
+
+		Integer[] attackerDicesResults = new Integer[attackerTanks];
+		Integer[] defenderDicesResults = new Integer[defenderTanks];
 		
-		int attackerTanksLost = 0;
-		int defenderTanksLost = 0;
-		int maxNumberOfRolls = 0;
-		
-		if (attackerTanks >= defenderTanks) {
-			maxNumberOfRolls = defenderTanks;
-		} else {
-			maxNumberOfRolls = attackerTanks;
+		for(int i = 0; i < attackerDicesResults.length; ++i) {
+			attackerDicesResults[i] = extractNumber();
 		}
 		
-		Integer[] attackerDicesResults = new Integer[maxNumberOfRolls];
-		Integer[] defenderDicesResults = new Integer[maxNumberOfRolls];
-		
-		for(int i = 0; i < maxNumberOfRolls; ++i) {
-			attackerDicesResults[i] = extractNumber();
+		for(int i = 0; i < defenderDicesResults.length; ++i) {
 			defenderDicesResults[i] = extractNumber();
 		}
-		
-		if (maxNumberOfRolls > 1) {
-			Arrays.sort(attackerDicesResults, Collections.reverseOrder());
-		}
 
-		for(int i = 0; i < maxNumberOfRolls; ++i) {
+		Arrays.sort(attackerDicesResults, Collections.reverseOrder());
+		Arrays.sort(defenderDicesResults, Collections.reverseOrder());
+		
+		List<Integer[]> results = new LinkedList<Integer[]>();
+		results.add(attackerDicesResults);
+		results.add(defenderDicesResults);
+		return compareDices(results);
+	
+	}
+	
+	public int[] compareDices(List<Integer[]> results) {
+		
+		Integer[] attackerDicesResults = results.get(0);
+		Integer[] defenderDicesResults = results.get(1);
+		int attackerTanksLost = 0;
+		int defenderTanksLost = 0;
+		
+		int numIterations = Math.min(attackerDicesResults.length, defenderDicesResults.length);
+		
+		for(int i = 0; i < numIterations; ++i) {
 			if (attackerDicesResults[i] > defenderDicesResults[i]) {
 				defenderTanksLost++;
 			} else {
@@ -42,12 +53,10 @@ public class Dice {
 			}
 		}
 		
-		if (defenderTanksLost != 0) {
-			
-		}
-		return 0;
-		//to be continued
-	
+		int[] tanksLost = new int[2];
+		tanksLost[0] = attackerTanksLost;
+		tanksLost[1] = defenderTanksLost;
+		
+		return tanksLost;
 	}
-	
 }
--- src/test/java/com/drisk/domain/AssignTanksPhaseTest.java
+++ src/test/java/com/drisk/domain/AssignTanksPhaseTest.java
@@ -5,11 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-<<<<<<< HEAD
 import com.drisk.domain.exceptions.SyntaxException;
-=======
-import com.drisk.technicalservice.SyntaxException;
->>>>>>> branch 'master' of git@github.com:UnimibSoftEngCourse1819/progetto-risiko-1-team1.git
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -17,9 +13,9 @@ public class AssignTanksPhaseTest {
 	
 	@Before
 	public void initialize() {
-		String s = "{'difficulty' : 'easy', 'continents' : ['africa', 'europe'], 'territories' : ['italy', 'france', 'egypt', 'north africa'],"
-				+ " 'membership' : [{'name' : 'europe', 'territories' : ['italy', 'france']}, {'name' : 'africa', 'territories' : ['egypt', 'north africa']}],"
-				+ " 'neighbourhood' : [{'name' : 'italy', 'territories' : ['france', 'egypt']}, {'name' : 'north africa', 'territories' : ['egypt']}]}";
+		String s = "{'difficulty' : 'easy', 'continents' : ['africa', 'europe', 'asia'], 'territories' : ['italy', 'france', 'egypt', 'north africa', 'kamchatka', 'china', 'japan', 'india', 'middle east'],"
+				+ " 'membership' : [{'name' : 'europe', 'territories' : ['italy', 'france']}, {'name' : 'africa', 'territories' : ['egypt', 'north africa']}, {'name' : 'asia', 'territories' : ['china', 'kamchatka', 'japan', 'india', 'middle east']}],"
+				+ " 'neighbourhood' : [{'name' : 'italy', 'territories' : ['france', 'egypt']}, {'name' : 'north africa', 'territories' : ['egypt']}, {'name' : 'china', 'territories' : ['india', 'japan']}, {'name' : 'middle east', 'territories' : ['india']}]}";
 		Gson json = new Gson();
 		JsonObject obj = json.fromJson(s, JsonObject.class); 
 		try {
@@ -37,19 +33,18 @@ public class AssignTanksPhaseTest {
 		assertEquals(1, player.getAvailableTanks());
 		player.removeAvailableTanks();
 
-		player.addTerritoryOwned(new Territory("south africa"));
-		player.addTerritoryOwned(new Territory("north africa"));
+		player.addTerritoryOwned(new Territory("italy"));
 		player.addTerritoryOwned(new Territory("egypt"));
-		player.addTerritoryOwned(new Territory("madagascar"));
-		player.addTerritoryOwned(new Territory("congo"));
-		player.addTerritoryOwned(new Territory("great britain"));
+		player.addTerritoryOwned(new Territory("japan"));
+		player.addTerritoryOwned(new Territory("kamchatka"));
+		player.addTerritoryOwned(new Territory("middle east"));
 		new AssignTanksPhase().assignTanks(player);
-		assertEquals(2, player.getAvailableTanks());
+		assertEquals(1, player.getAvailableTanks());
 		player.removeAvailableTanks();
 		
-		player.addTerritoryOwned(new Territory("east africa"));
+		player.addTerritoryOwned(new Territory("india"));
 		new AssignTanksPhase().assignTanks(player);
-		assertEquals(5, player.getAvailableTanks());
+		assertEquals(2, player.getAvailableTanks());
 		player.removeAvailableTanks();
 		
 	}
@@ -62,23 +57,14 @@ public class AssignTanksPhaseTest {
 		int numberOfTanks = new AssignTanksPhase().getTanksPerContinent(player);
 		assertEquals(0, numberOfTanks);
 		
-		player.addTerritoryOwned(new Territory("south africa"));
 		player.addTerritoryOwned(new Territory("north africa"));
 		player.addTerritoryOwned(new Territory("egypt"));
-		player.addTerritoryOwned(new Territory("madagascar"));
-		player.addTerritoryOwned(new Territory("congo"));
-		player.addTerritoryOwned(new Territory("east africa"));
 		
 		numberOfTanks = new AssignTanksPhase().getTanksPerContinent(player);
 		assertEquals(3, numberOfTanks);
 		
-		player.addTerritoryOwned(new Territory("great britain"));
-		player.addTerritoryOwned(new Territory("ukraine"));		
-		player.addTerritoryOwned(new Territory("iceland"));
-		player.addTerritoryOwned(new Territory("north europe"));
-		player.addTerritoryOwned(new Territory("south europe"));
-		player.addTerritoryOwned(new Territory("scandinavia"));
-		player.addTerritoryOwned(new Territory("west europe"));
+		player.addTerritoryOwned(new Territory("italy"));		
+		player.addTerritoryOwned(new Territory("france"));
 		numberOfTanks = new AssignTanksPhase().getTanksPerContinent(player);
 		assertEquals(8, numberOfTanks);	
 		
