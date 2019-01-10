22021e1db21c418fe44001d899ec1f249403d38c
SimoneMottadelli <s.mottadelli2@campus.unimib.it>
2019-01-11 00:24:59 +0100

    Updated front-end and back-end of Game client page

--- src/main/webapp/javascript/game.js
+++ src/main/webapp/javascript/game.js
@@ -1,117 +1,124 @@
 $(document).ready(function(){
 	
+	// variables that hold important information of the game
 	var myColor = null;
-	var myTerritories = null;
-	var availableTanks = null;
-	showOrHidePlaceTanksDiv(false);
-	findMyColor();
+	var numberOfAvailableTanks = null;
+	var map = {'territories': [], 'neighbourhood': []};
 	
-	$('#placeTanksButton').click(function() {
-		initialTanksPlacement();
+	
+	$("#placeTanksButton").click(function() {
+		placeTanks();
 	});
 	
-	// used to show or hide a div - val = false -> hide, val = true -> show
-	function showOrHidePlaceTanksDiv(val){
-		var div = $('#placeTanksDiv')
-		if(val == false)
-			div.hide();
-		else
-			div.show();
-	}
+
+	setColorAndFirstAvailableTanks();
+	fillSVGMapDiv();
+	requestMapLoading();
 	
-	function showOrHideGameDiv(val) {
-		var div = $('#gameDiv');
-		if(val == false)
-			div.hide();
-		else
-			div.show();
-	}
 	
-	// used to get this player's color and to identify it on this page
-	function findMyColor() {
-		$.getJSON('../game/getColorFromSession', function(data) {
-			if(data.responseCode != -1) {
-				myColor = data.responseMessage;
-				getPlayerInfo();
-				loadMap();
-				showOrHidePlaceTanksDiv(true);
+	function setColorAndFirstAvailableTanks() {
+		$.getJSON('../game/playerInfo', function(result) {
+			if(result.responseCode != -1) {
+				myColor = result.color;
+				numberOfAvailableTanks = result.availableTanks;
 			}
 			else
-				alert(data.responseMessage);
+				showModalWindow(result.responseMessage);
 		});
 	}
 	
+	// fillSVGMapDiv is used to show the map return by the server on the web page.
+	function fillSVGMapDiv(){
+		$.getJSON('../game/mapImage', function(result) {
+			if(result.responseCode != -1) {
+				$("#mapDiv").html(result.mapSVG);
+				addMouseoverEventToTerritories();
+			}
+			else
+				showModalWindow(data.responseMessage);
+		});
+	}
+	
+	// requestMapLoading creates a link to connect the client to the server, so that the
+	// server can send map updates using Server Send Event mechanism.
+	// It also starts the initial tank placement phase
+	function requestMapLoading() {
+		var initialTankPlacementPhaseStarted = false;
+		var source = new EventSource('../game/map');
+		source.onmessage = function(event) {
+			map.territories = JSON.parse(event.data).territories;
+			map.neighbourhood = JSON.parse(event.data).neighbourhood;
+			if (!initialTankPlacementPhaseStarted && myColor != null) {
+				initialTankPlacementPhaseStarted = true;
+				updateMyTerritoriesSelect();
+				startTankPlacementPhase();
+			}	
+			updateMap();
+		}
+	}
+	
+	function updateMap() {
+		$("path.country").each(function() {
+			for (var i = 0; i < map.territories.length; ++i) {
+				if (map.territories[i].name == $(this).attr("id")) {
+					$(this).removeClass().attr("class", "country").addClass(map.territories[i].owner);
+					// modifica contatore carri sul territorio
+				}
+			}
+		});
+	}
+	
+	function updateMyTerritoriesSelect() {
+		$("#myTerritoriesSelect").html("");
+		for (var i = 0; i < map.territories.length; ++i) {
+			if (map.territories[i].owner == myColor)
+				$("#myTerritoriesSelect").append('<option value="' + map.territories[i].name + '">' 
+					+ map.territories[i].name + "</option>");
+		}
+	}
+	
+	function startTankPlacementPhase() {
+		$("#availableTanksLabel").html(numberOfAvailableTanks);
+		$("#placeTanksDiv").show();
+	}
+
+	
 	function getPlayerInfo() {
-		$getJSON('../game/playerInfo', function(data) {
+		$.getJSON('../game/playerInfo', function(data) {
 			if(data.responseCode != -1) {
 				
 			}
 			else
-				alert(data.responseMessage);
+				showModalWindow(data.responseMessage);
 		});
 	}
 	
-	// used to load map structure every time that the server send it
-	function loadMap() {
-		var source = new EventSource('../game/map');
-		source.onmessage = function(event) {
-			var continents = JSON.parse(event.data).continents;
-			continents = JSON.stringify(continents);
-			var territories = JSON.parse(event.data).territories;
-			myTerritories = territories;
-			territories = JSON.stringify(territories);
-			var membership = JSON.parse(event.data).membership;
-			membership = JSON.stringify(membership);
-			var neighbourhood = JSON.parse(event.data).neighbourhood;
-			neighbourhood = JSON.stringify(neighbourhood);
-			showMap(continents, territories, membership, neighbourhood);
-		}
-	}
 	
-	// called only in initial game phase
-	function initialTanksPlacement() {
-		var territory = $('#from').val();
-		$('#from').val('');
-		var numOfTanks = parseInt($('#howMuch').val(), 10);
-		$('#howMuch').val(0);
-		if(isMyTerritory(territory)) {
-			$.ajax({
-				type : 'POST',
-				url : '../game/initialTanksPlacement',
-				contentType: 'application/json',
-				dataType: 'json',
-				data: JSON.stringify(placeTanksJson(territory, numOfTanks)), 
-				success: function(data) {
-					alert(data.responseMessage);
+	// function used to place tanks to a territory
+	function placeTanks() {
+		var territory = $("#myTerritoriesSelect").val();
+		var numOfTanks = parseInt($('#howMany').val(), 10);
+		$('#howMany').val(0);
+		$.ajax({
+			type : 'POST',
+			url : '../game/initialTanksPlacement',
+			contentType: 'application/json',
+			dataType: 'json',
+			data: JSON.stringify(placeTanksJson(territory, numOfTanks)), 
+			success: function(data) {
+				if (data.responseCode == -1)
+					showModalWindow(data.responseMessage);
+				else {
+					numberOfAvailableTanks -= numOfTanks;
+					$("#availableTanksLabel").html(numberOfAvailableTanks);
 				}
-			});
-		}
-		else
-			alert(territory + ' is not yours');
+			}
+		});
 	}
 	
 	// helper function that return json object
 	function placeTanksJson(territory, numOfTanks) {
 		return {'where' : territory, 'numOfTanks' : numOfTanks};
-	}
-	
-	// helper function to avoid server further work
-	function isMyTerritory(territoryName) {
-		var i = 0;
-		while(i < myTerritories.length) {
-			if(myTerritories[i].name == territoryName && myTerritories[i].owner == myColor)
-				return true;
-			++i;
-		}
-		return false;
-	}
-	
-	// used to refresh html page with che real structure of the map
-	function showMap(continents, territories, membership, neighbourhood) {
-		$('#continentsDiv').html(continents);
-		$('#territoriesDiv').html(territories);
-		$('#membershipDiv').html(membership);
-		$('#neighbourhoodDiv').html(neighbourhood);
 	}
 	
 	// used to get information of turn status from the server, invoked only one time
@@ -127,8 +134,8 @@ $(document).ready(function(){
 						if(players[i].color == myColor) {
 							availableTanks = players[i].availableTanks
 							if(availableTanks == 0) {
-								showOrHidePlaceTanksDiv(false);
-								alert("all tanks placed, wait that all players place their tanks");
+								$("#gameDiv").attr("class", "hidden");
+								showModalWindow("all tanks placed, wait that all players place their tanks");
 								return;
 							}
 						}
@@ -139,9 +146,30 @@ $(document).ready(function(){
 			else {
 				var currentPlayer = JSON.parse(event.data).currentPlayer;
 				if(myColor != currentPlayer)
-					showOrHideGameDiv(false);
+					$("#gameDiv").attr("class", "hidden");
 			}
 		}
 	}
+	
+	function addMouseoverEventToTerritories() {
+		$("path.country").each(function() {
+			$(this).hover(
+					function() {
+						$("#territoryNameLabel").html($(this).attr("id"));
+					}, 
+					function() {
+						$("#territoryNameLabel").html("");
+					});
+		});
+	}
+	
+	function showModalWindow(message) {
+		$("div.modal-body").html("<h2>" + message + "</h2>");
+		$("#modalWindow").css("display", "block");
+	}
+	
+	$("span.close").click(function() {
+		$("#modalWindow").css("display", "none");
+	});
 	
 });
