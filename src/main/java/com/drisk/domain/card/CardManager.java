d89b154c1bfac1bfeae04057dbe8d07ab89e6812
Matteo Paolella <m.paolella1@campus.unimib.it>
2019-01-12 09:36:27 +0100

    Implemented "Multiple Mission" feature in CardManager

--- src/main/java/com/drisk/domain/card/CardManager.java
+++ src/main/java/com/drisk/domain/card/CardManager.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import com.drisk.domain.game.ObjectiveTypeEnum;
 import com.drisk.domain.game.Player;
-import com.drisk.domain.map.DifficultyEnum;
+import com.drisk.domain.map.Continent;
 import com.drisk.domain.map.MapManager;
 import com.drisk.domain.map.Territory;
 
@@ -16,118 +16,129 @@ public class CardManager {
 	private List<Card> territoryCards;
 	private List<Card> discardedCards;
 	private static CardManager instance;
-
+	
 	private CardManager() {
 		missionCards = new LinkedList<>();
 		territoryCards = new LinkedList<>();
 		discardedCards = new LinkedList<>();
 	}
-
+	
 	public static CardManager getInstance() {
 		if (instance == null)
 			instance = new CardManager();
 		return instance;
+	}	
+	
+	public void initPlayersMission(List<Player> players) {	
+		for(int i = 0; i < players.size(); ++i) 
+			players.get(i).setMission((MissionCard) missionCards.get(i % missionCards.size()));
 	}
-
-	public void initPlayersMission(List<Player> players, ObjectiveTypeEnum objective) {
-		if (objective.equals(ObjectiveTypeEnum.COMMON)) {
-			MissionCard mission = (MissionCard) missionCards.get(0);
-			for (Player p : players)
-				p.setMission(mission);
-		} else {
-			// missione non singola, and so objective.equals(ObjectiveTypeEnum.MULTIPLE)
-		}
-	}
-
+	
 	public TerritoryCard findTerritoryCardByTerritoryName(String territoryName) {
 		for (Card tc : territoryCards)
-			if (((TerritoryCard) tc).getTerritory().getName().equals(territoryName))
+			if (((TerritoryCard)tc).getTerritory().getName().equals(territoryName))
 				return (TerritoryCard) tc;
 		for (Card tc : discardedCards)
-			if (((TerritoryCard) tc).getTerritory().getName().equals(territoryName))
+			if (((TerritoryCard)tc).getTerritory().getName().equals(territoryName))
 				return (TerritoryCard) tc;
 		return null;
 	}
-
-	public void initCards() {
+	
+	public void initCards(List<Player> players, ObjectiveTypeEnum objective) {
 		initTerritoryCards();
-		initMissionCards(MapManager.getInstance().getMapDifficulty());
+		initMissionCards(players, objective);
 		shuffleDeck(territoryCards);
 		shuffleDeck(missionCards);
 	}
-
+	
 	public void initTerritoryCards() {
 		TerritoryCardSymbolEnum[] symbols = TerritoryCardSymbolEnum.values();
 		int i = 0;
-		for (Territory t : MapManager.getInstance().getMapTerritories()) {
+		for(Territory t : MapManager.getInstance().getMapTerritories()) {
 			TerritoryCard tc = new TerritoryCard(t, symbols[i % 3]);
 			addTerritoryCard(tc);
 			++i;
 		}
 	}
-
+	
 	private void addTerritoryCard(TerritoryCard tc) {
-		if (!territoryCards.contains(tc))
+		if(!territoryCards.contains(tc))
 			territoryCards.add(tc);
 	}
-
-	public void initMissionCards(DifficultyEnum dif) {
-		switch (dif) {
-		case EASY:
-			MissionCard missionEasy = new ConquerTerritoryMissionCard(14);
-			missionCards.add(missionEasy);
-			break;
-		case MEDIUM:
-			MissionCard missionMedium = new ConquerTerritoryMissionCard(19);
-			missionCards.add(missionMedium);
-			break;
-		case HARD:
-			MissionCard missionHard = new ConquerTerritoryMissionCard(24);
-			missionCards.add(missionHard);
-			break;
-		default:
-			MissionCard missionCustom = new ConquerTerritoryMissionCard(10);
-			missionCards.add(missionCustom);
+	
+	public void initMissionCards(List<Player> players, ObjectiveTypeEnum objective) {
+		if(objective.equals(ObjectiveTypeEnum.MULTIPLE)) {
+			missionCards.addAll(createConqerContinentMission());
+			missionCards.addAll(createDestroyEnemyMission(players));
 		}
+		
+		missionCards.add(new ConquerTerritoryMissionCard((MapManager.getInstance().getNumberOfTerritories()*4)/7));
 	}
 
-	private void shuffleDeck(List<Card> cards) {
+	private List<MissionCard> createConqerContinentMission() {
+		
+		Continent asia = MapManager.getInstance().findContinentByName("asia");
+		Continent africa = MapManager.getInstance().findContinentByName("africa");
+		Continent north_america = MapManager.getInstance().findContinentByName("north_america");
+		Continent south_america = MapManager.getInstance().findContinentByName("south_america");
+		Continent australia = MapManager.getInstance().findContinentByName("australia");
+		
+		List<MissionCard> missions = new LinkedList<>();
+		
+		missions.add(new ConquerContinentMissionCard(asia, africa));
+		missions.add(new ConquerContinentMissionCard(asia, south_america));
+		missions.add(new ConquerContinentMissionCard(north_america, africa));
+		missions.add(new ConquerContinentMissionCard(north_america, australia));
+		
+		return missions;
+	}
+
+	private List<MissionCard> createDestroyEnemyMission(List<Player> players) {
+		List<MissionCard> missions = new LinkedList<>();
+		
+		for(Player p : players)
+			missions.add(new DestroyEnemyMissionCard(p));
+			
+		return missions;
+	}
+	
+	public void shuffleDeck(List<Card> cards) {
 		Collections.shuffle(cards);
 	}
-
+	
 	public void giveTerritoryCard(Player player) {
 		player.addTerritoryCards((TerritoryCard) drawCard(territoryCards));
-		if (territoryCards.isEmpty())
-			refillDeck();
 	}
-
+	
 	private Card drawCard(List<Card> cards) {
 		return cards.remove(0);
 	}
-
-	private void refillDeck() {
-		setTerritoryCards(discardedCards);
-		discardedCards.clear();
-		shuffleDeck(territoryCards);
+	
+	public void refillDeck() {
+		if(territoryCards.isEmpty()) {
+			setTerritoryCards(discardedCards);
+			discardedCards.clear();
+			shuffleDeck(territoryCards);
+		}
 	}
-
+	
 	public void removeCards(Player player, TerritoryCard[] tris) {
 		for (int i = 0; i < tris.length; ++i)
 			removeCard(player, tris[i]);
 	}
 
+	public void removeCards(Player player, List<TerritoryCard> territoryCards) {
+		for(TerritoryCard tc : territoryCards)
+			removeCard(player, tc);
+	}
+	
 	public void removeCard(Player player, TerritoryCard card) {
-		if (player.getTerritoryCardsHand().contains(card)) {
+		if(player.getTerritoryCardsHand().contains(card)) {
 			player.getTerritoryCardsHand().remove(card);
 			discardedCards.add(card);
 		}
 	}
 	
-	public void removeCards(Player player, List<TerritoryCard> territoryCards) {
-		for(TerritoryCard tc : territoryCards)
-			removeCard(player, tc);
-	}
-
 	public List<Card> getMissionCards() {
 		return missionCards;
 	}
@@ -135,18 +146,27 @@ public class CardManager {
 	public List<Card> getTerritoryCards() {
 		return territoryCards;
 	}
-
+	
 	public List<Card> getDiscardedCards() {
 		return discardedCards;
 	}
 
-	private void setTerritoryCards(List<Card> cards) {
+	public void setDiscardedCards(List<Card> cards) {
+		this.discardedCards.clear();
+		this.discardedCards.addAll(cards);
+	}
+
+	public void setTerritoryCards(List<Card> cards) {
 		this.territoryCards.clear();
 		this.territoryCards.addAll(cards);
 	}
 
-	public static void destroy() {
-		instance = null;
+	public void setMissionCards(List<Card> cards) {
+		this.missionCards.clear();
+		this.missionCards.addAll(cards);
 	}
 
+	public static void destroy() {
+		instance = null;
+	}	
 }
--- src/main/java/com/drisk/domain/card/ConquerContinentMissionCard.java
+++ src/main/java/com/drisk/domain/card/ConquerContinentMissionCard.java
@@ -24,5 +24,6 @@ public class ConquerContinentMissionCard extends MissionCard {
 				return false;
 		return true;
 	}
+
 	
 }
